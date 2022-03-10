c3f3288f7fd3e9dc88bcc74944cf1f580d70e275
shubh <shubhamganeshsonawane@gmail.com>
2022-03-10 16:49:30 +0530

    Adding Frist repo

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace myproject
+{
+    class Program
+    {
+        static void Main(string[] args)//function
+        {
+            //Console.Write("Hello World! ");//print the line
+            Console.WriteLine("My name is Shubham");//print the secound line
+            // Console.ReadLine();//console print Read line time 
+
+            //comment in c#....use // && /*..*/
+
+            //int integer = 34;//Integer variable
+            //Console.WriteLine("The number is the "+ integer);
+            //string s = Console.ReadLine();//write a Input in program
+            //Console.WriteLine(s);//print the output in the terminal
+
+            /*
+            Data type in c#
+            Integer - int = 123 ;
+            longInteger - long= 874963276
+            Floating - float = 0.69876;
+            double - double = 0.78;
+            Character - char = A;
+            Boolen - bool = true;
+            String - string "shubham";
+            */
+
+            
+            int a = 35;
+            double b = 34.5D;
+            float c = 32.2f;
+            bool isGreat = true;
+            char d ='r';
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(isGreat);
+            Console.WriteLine(d);
+
+
+
+        }
+    }
+}
